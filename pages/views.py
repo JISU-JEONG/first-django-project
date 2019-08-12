2b351c8d538215e7c5e9dba4aa7c7b4fec27059e
JISU_JEONG <jindex2411@naver.com>
2019-08-12 16:15:13 +0900

    마크다운 정리

--- pages/views.py
+++ pages/views.py
@@ -39,3 +39,21 @@ def dinner(request):
         'google_link' : 'http://www.google.com',
     }
     return render(request, 'dinner.html', context)
+
+def cube(request, num):
+    context= { 
+        'num' : num**3,
+        'numbers' : [1, 2, 3, 4, 5],
+        'student' : {
+            '지수' : '지수!',
+            '태수' : '태수!' 
+        }
+    }
+    return render(request, 'cube.html', context)
+
+def about(request, name, age):
+    context= {
+        'name': name,
+        'age' : age
+    }
+    return render(request, 'about.html', context)
